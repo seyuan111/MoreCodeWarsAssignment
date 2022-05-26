bf4c231ba808ad43b7962b236e201789d7a2c46e
Sean Yuan <ysean54@yahoo.com>
2022-05-25 22:07:09 -0400

    more challenges added today

--- Codewars.js
+++ Codewars.js
@@ -291,3 +291,45 @@ function duplicates(array){
   }
   return count
 }
+
+//I love you, a little , a lot, passionately ... not at all
+
+//Who remembers back to their time in the schoolyard, when girls would take
+//a flower and tear its petals, saying each of the following phrases each
+//time a petal was torn:
+
+//I love you
+//a little
+//a lot
+//passionately
+//madly
+//not at all
+//When the last petal was torn there were cries of excitement, dreams,
+//surging thoughts and emotions.
+
+//Your goal in this kata is to determine which phrase the girls would say
+//for a flower of a given number of petals, where nb_petals > 0.
+
+function howMuchILoveYou(nbPetals) {
+    
+  let arr = ["I love you","a little","a lot","passionately","madly","not at all"]
+  
+  for(let i = 0; i <= nbPetals; i++){
+      arr.push(arr[i])
+  }
+return arr[nbPetals-1]
+}
+
+//Convert number to reversed array of digits
+
+//Given a random non-negative number, you have to return the digits of this
+//number within an array in reverse order.
+
+//Example:
+//348597 => [7,9,5,8,4,3]
+
+function digitize(n) {
+  let str = String(n)
+  
+  return str.split('').map(Number).reverse()
+}
