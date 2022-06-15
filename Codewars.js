33e94b2416469c593b1a9cd8970229e3e685e9c0
Sean Yuan <ysean54@yahoo.com>
2022-06-14 22:21:50 -0400

    did another codewars challenge

--- Codewars.js
+++ Codewars.js
@@ -378,3 +378,12 @@ function ghostBusters(building) {
     return "You just wanted my autograph didn't you?"
   }
 }
+
+//Fake Binary
+
+//Given a string of digits, you should replace any digit below 5 with '0'
+//and any digit 5 and above with '1'. Return the resulting string.
+
+function fakeBin(x){
+  return x.split('').map((num) => num > 4 ? 1 : 0).join('')
+ }
