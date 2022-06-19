e86cbd44688c211d5c22ef71850ee56be2341429
Sean Yuan <ysean54@yahoo.com>
2022-06-19 14:15:50 -0400

    another codewars pushed

--- Codewars.js
+++ Codewars.js
@@ -387,3 +387,33 @@ function ghostBusters(building) {
 function fakeBin(x){
   return x.split('').map((num) => num > 4 ? 1 : 0).join('')
  }
+
+//Expressions Matter
+
+//Given three integers a ,b ,c, return the largest number obtained after
+//inserting the following operators and brackets: +, *, ()
+//In other words, try every combination of a,b,c with [*+()] , and return
+//the Maximum Obtained
+
+//Example
+//With the numbers are 1, 2 and 3 , here are some ways of placing signs
+//and brackets:
+
+//1 * (2 + 3) = 5
+//1 * 2 * 3 = 6
+//1 + 2 * 3 = 7
+//(1 + 2) * 3 = 9
+
+//So the maximum value that you can obtain is 9.
+
+function expressionMatter(a, b, c) {
+  let value = Math.max(
+    a + b + c,
+    a * b * c,
+    a * b + c,
+    a * (b + c),
+    a + b * c,
+    (a + b) * c
+  )
+  return value
+}
