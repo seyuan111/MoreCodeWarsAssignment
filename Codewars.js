0a184ac82a4ccb085abbf6ae3d55a9aca00acaab
Sean Yuan <ysean54@yahoo.com>
2022-10-15 21:42:24 -0400

    another codewars added

--- Codewars.js
+++ Codewars.js
@@ -727,3 +727,24 @@ function solution(str){
 function nearestSq(n){
   return Math.pow(Math.round(Math.sqrt(n)), 2)
 }
+
+//Sorting Dictionaries
+
+//Python dictionaries are inherently unsorted. So what do you do if you
+//need to sort the contents of a dictionary?
+
+//Create a function that returns a sorted list of (key, value) tuples
+//(Javascript: arrays of 2 items).
+
+//The list must be sorted by the value and be sorted largest to smallest.
+
+function sortDict(dict) {
+  let arr = []
+  for (let key in dict){
+    let newarr = []
+    newarr.push(+key || key)
+    newarr.push(dict[key])
+    arr.push(newarr)
+  }
+  return arr.sort((a,b)=>a[1]>b[1]?-1:1)
+}
