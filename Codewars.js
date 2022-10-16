9996160f8300eead2e3afdc1ebd635bce0a22ba9
Sean Yuan <ysean54@yahoo.com>
2022-10-16 18:11:21 -0400

    another codewars added again

--- Codewars.js
+++ Codewars.js
@@ -748,3 +748,18 @@ function sortDict(dict) {
   }
   return arr.sort((a,b)=>a[1]>b[1]?-1:1)
 }
+
+//All Star Code Challenge #18
+
+//Create a function that accepts 2 string arguments and returns an
+//integer of the count of occurrences the 2nd argument is found in the
+//first one.
+
+//If no occurrences can be found, a count of 0 should be returned.
+
+//Test.assertEquals(strCount('Hello', 'o'), 1);
+//Test.assertEquals(strCount('Hello', 'l'), 2);
+
+function strCount(str, letter){  
+  return str.split(letter).length -1
+}
