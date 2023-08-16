e3d7be5a2f34e73558b5dcb37ed2acda3385a546
Sean Yuan <ysean54@yahoo.com>
2023-08-15 22:47:24 -0400

    1 more codewars added

--- Codewars.js
+++ Codewars.js
@@ -62,6 +62,18 @@ function roundToNext5(n){
     return Math.ceil(n/5) * 5;
 }
 
+//Find numbers which are divisible by given number
+
+//Complete the function which takes two arguments and returns all numbers
+//which are divisible by the given divisor. First argument is an array of
+//numbers and the second is the divisor.
+
+//[1, 2, 3, 4, 5, 6], 2 --> [2, 4, 6]
+
+function divisibleBy(numbers, divisor){
+  return numbers.filter((number) => number % divisor === 0)
+}
+
 //Rock paper scissors
 
 const rps = (p1, p2) => {
